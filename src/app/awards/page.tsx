e4278adb8fd5e20e5a45d7a409066c1461cb31d8
gitaehee <rainwifekth@naver.com>
2025-02-14 12:16:46 +0900

    feat: 헤더랑 페이지 등 초기 설정

---
+++ src/app/awards/page.tsx
@@ -0,0 +1,13 @@
+export default function Awards() {
+    
+    return (
+      <div>
+        <h4 className="title">시상식 페이지</h4> {
+            <div className="image1">
+                <h4 className="message">이미지 넣을 거임</h4>
+            </div>
+        }
+      </div>
+    );
+  }
+  
---
+++ src/app/vote/page.tsx
@@ -0,0 +1,13 @@
+export default function Vote() {
+    
+    return (
+      <div>
+        <h4 className="title">투표 페이지</h4> {
+            <div className="image1">
+                <h4 className="message">이미지 넣을 거임</h4>
+            </div>
+        }
+      </div>
+    );
+  }
+  
